21784eb29353c52b86fe85999910176c349c1807
leekwonhyo <kensi0512@gmail.com>
2020-06-30 21:14:53 +0900

    Play the game

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,10 +1,11 @@
+from random import randint
+
 # list of play options
 play = ["Rock", "Paper", "Scissors"]
 
-# assign a random play to the computer
-computer = "Rock"
-print('Computer: {}'.format(computer))
+# get the user input
+player = input("Rock, Paper, Scissors? ")
 
 # get the user input
-player = "Paper"
-print('Player: {}'.format(player))
+computer = play[randint(0, 2)]
+print('computer: {}'.format(computer))
